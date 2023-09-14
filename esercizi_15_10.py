e008359c241091db07c0f27821e3c3c2fd8379c3
Andrea marini <34715958+AndrewMarines@users.noreply.github.com>
2023-09-14 12:19:49 +0000

    Create esercizi_15_10.py

---
+++ esercizi_15_10.py
@@ -0,0 +1,21 @@
+#1. Calcolare area di un cerchio basandosi sul raggio dato dall'utente;
+#2. Chiedere all'utente nome e cognome e riscriverli entrambi al contrario(es. Andrea Marini->iniraM aerdnA);
+#3. Chiedere all'utente un numero. Fare la sottrazione tra 17 e il numero dell'utente. Se il numero e' negativo, renderlo positivo!(e' il val. assoluto di un numero);
+#4. Chiedere all'utente un numero. Controllare se questo e' compreso tra 1 e 100;
+#5. Chiedere all'utente un numero. Verificare se questo e' pari o dispari. Ricordate operatore modulo;
+#6. x="Pallino", y="Pinco". Far diventare x="Pinco", y="Pallino";
+
+
+#Chi esegue per primo tutti e 6 gli esercizi avra' 3 +. Il secondo 2, Il terzo 1. 
+
+
+
+"""
+You've stumbled upon a door where your mind is the key.
+There are none who will lend you guidance;
+These trials are yours to conquer alone.
+Entering here will take more than mere logic and strategy,
+but the criteria are just as hidden as what they reveal.
+This is a mirage.
+"""
+
