ec804657e053cc1a7bed0875a65e99858991f401
Andrea marini <34715958+AndrewMarines@users.noreply.github.com>
2023-09-24 10:25:46 +0000

    Create esercizi_aggiuntivi_for&liste.py

---
+++ esercizi_aggiuntivi_for&liste.py
@@ -0,0 +1,28 @@
+#1 Chiedi ad utente di inserire una qualsiasi parola. Fai lo spelling automatico di questa parola.
+"""
+Esempio: Andrea
+A
+n
+d
+r
+e
+a
+"""
+
+#2 Crea una lista di almeno 10 numeri interi. Fare l'algoritmo bubble sort di questa lista
+#Lo avevamo gia' fatto con algobuild. Guarda un video su youtube che te lo spiega facilmente
+
+#3 Crea 2 liste di almeno 7 elementi numeriche. In una terza lista metti solo i valori massimi comparati tra le due liste
+"""
+lista1=[0,1,2,3,4,5,6]
+lista2=[1,2,5,2,6,3,8]
+lista_max=[1,2,5,3,4,5,8]
+"""
+
+#4 Crea una matrice(lista) bidimensionale e stampala a schermo formattata decentemente
+"""
+Una lista e' cosi' [0,7,5,3]
+Una lista bidimensionale e' una roba tipo
+[0,5,2,1,
+ 3,1,5,7,
+ 2,2,1,6]
