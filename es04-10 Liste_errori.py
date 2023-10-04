727cb193a8c38891f57940468f2ccd28783f05e1
Andrea marini <34715958+AndrewMarines@users.noreply.github.com>
2023-10-04 08:47:47 +0000

    Add files via upload

---
+++ es04-10 Liste_errori.py
@@ -0,0 +1,52 @@
+#es.1
+"""
+Date le seguenti variabili:
+    somm = 3000
+    c = 0
+Dividi la variabile somm per la variabile contatore. La variabile contatore può cambiare e assumere valori diversi. Usando il try e except Python gestisci la divisione per zero. Se la divisione è fatta correttamente, stampa il risultato sulla console. Se si verifica un errore, stampa sulla console: Divisione per zero.
+"""
+
+#es.2
+"""
+Viene fornito un dizionario:
+    users = {'001': 'Marco', '002': 'Monica', '003': 'Giovanni'}
+Provare a stampare il valore della chiave user_id = '004'.
+
+In caso di errore di KeyError, stampare sulla console:
+
+004 non è nel dizionario...
+"""
+
+
+#es.3
+"""
+lista=[1,2,3]
+chiedere all'utente se vuole inserire dei dati o stampare la lista completa(in questo caso far finire il programma).
+I DATI CHE L'UTENTE INSERISCE DEVONO ESSERE PER FORZA NUMERI INTERI!!!
+Come posso verificare se un input è un numero intero o meno in Python? Forse questa ricerca su stackoverflow può darvi qualche risultato?!
+"""
+
+#es.4
+"""
+lista=[8,4,2,6,8,3,7,2]
+fare fin quando la lista è vuota queste operazioni:
+Moltiplica il primo elemento per l'ultimo, cancella ultimo elemento della lista.
+Ogni volta che fai la moltiplicazione sommarla alla moltiplicazione precedente.
+Esempio: 
+1a moltiplicazione uguale a 5
+somma=0+5
+2a moltiplicazione uguale a 6
+somma = somma(che vale 5)+6
+3a moltiplicazione uguale a 10
+somma = somma(che vale 11)+10
+...
+"""
+
+#es.5
+"""
+lista=[1,5,8,1,5,7,6,8]
+Eliminare dalla lista tutti gli elementi doppi
+la lista finale deve risultare lista=[1,5,8,7,6]
+
+Ordinare questa lista(non è necessario fare l'ordinamento a mano, python ha già la sua funzione per farlo...)
+"""
