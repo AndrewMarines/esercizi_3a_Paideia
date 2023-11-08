a072ca636b71a7c4e1497d7aac1f588bf3acb573
Andrea marini <34715958+AndrewMarines@users.noreply.github.com>
2023-11-08 09:25:02 +0100

    Create es_classi.py

---
+++ es_classi.py
@@ -0,0 +1,27 @@
+#es1 [1+]
+"""
+Creare una classe Persona che abbia i seguenti attributi: nome, genere, anno nascita.
+Aggiungi un metodo “presentati” che stampi una frase di presentazione della persona, ad esempio “Ciao, mi chiamo Fena e sono nato nel 2001”.
+"""
+
+#es2[1+]
+"""
+Creare una classe Giocatore che abbia i seguenti attributi: Nome, squadra, ruolo, gol segnati, assist fatti.
+Aggiungi un metodo che stampi nome e ruolo;
+Aggiungi un metodo che stampi le statistiche.
+"""
+
+#es3 [2+]
+"""
+Creare una lista di giocatori usando la classe Giocatore precedente.
+Trovare il giocatore che ha segnato più gol ed il giocatore che ha eseguito più assist.
+"""
+
+#es4[5+]
+"""
+Creare una classe Squadra che abbia i seguenti attributi: nome, anno di nascita, lista di giocatori(potete usare la classe dell'es2) nella squadra.
+Aggiungi un metodo che ti permetta di aggiungere un giocatore alla lista;
+Aggiungi un metodo che ti dica il capocannoniere della squadra;
+Aggiungi un metodo che ti dica l'assist-man della squadra;
+Aggiungi un metodo che faccia una presentazione ordinata degli elementi della squadra.
+"""
